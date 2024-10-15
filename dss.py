bd833fe44b66c3169a930b27c3d10fe850aac27f
infultMK2 <infernusultimo@gmail.com>
2024-10-15 00:03:48 -0400

    added response list and response cycle when word is said

--- dss.py
+++ dss.py
@@ -1,13 +1,19 @@
-
 import discord
-from discord_token import TOKEN
+import random 
 
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
 intents.message_content = True
 
+token = os.getenv("DISCORD_TOKEN")
 client = discord.Client(intents=intents)
+
+response_list = {
+    "hawk": [f'*hawk..?*', "hawk tuah! ~~spit on that thing~~ kill yourself!", "hawk? why don't you talk tuah some bitches?"],
+    "crazy": ["crazy? i was crazy once. they locked me in a room. a rubber room. a rubber room with rats. and rats make me crazy.", "y'know what would be crazier tho? touching some grass"],
+    "mn" : [f'MN mentioned!! what the fuck are "food options" rahhh!!']
+}
 
 @client.event
 async def on_ready():
@@ -19,8 +25,14 @@ async def on_message(message):
     if message.author == client.user: 
         return
     
-    if message.content == 'hello':
-        await message.channel.send(f'Sup {message.author}')
+    if "hawk" in message.content:
+        await message.channel.send(f'*`did someone say..?`*', reference=message)
+
+    for word, responses in response_list.items():
+        if word in message.content.lower():
+            response = random.choice(responses)
+            await message.channel.send(response, reference=message)
+            return
 
 
 client.run(TOKEN)
